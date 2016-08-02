3a41c5812895f59a8bd581747e6fe9881f68f1ea
Ryan Jordan <ryan.jordan@gmail.com>
2016-08-01 23:00:28 -0400

    use https for marmalade

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 (require 'package)
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
-	("marmalade" . "http://marmalade-repo.org/packages")
+	("marmalade" . "https://marmalade-repo.org/packages")
 	("melpa" . "http://melpa.org/packages/")))
 (package-initialize)
 
